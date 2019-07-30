250295bf24f240e56e1a95b5e74772a4aac5cc69
Mayank Jain <jain15mayank@gmail.com>
2019-07-30 22:16:13 +0100

    Utility for natural perturbation addition and new adversarial agent code in progress

---
+++ Final Code/adversarialAgentNew.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  30 15:15:21 2019
+
+@author: Mayank Jain
+"""
+
+import os
+import cv2
+from copy import deepcopy
+import numpy as np
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import CustomObjectScope
+from tensorflow.keras.initializers import glorot_uniform
+
+from pso_pyswarm_mudSplat import pso
+
+from utils_mudSlap import *
+
+'''
+LOAD DATA
+'''
+def load_images_from_folder(folder, maxImg=None):
+  # Load 1 file to check image shape
+  i = 0
+  for filename in os.listdir(folder):
+    if i==0:
+      img = cv2.imread(os.path.join(folder,filename))
+      i+=1
+    else:
+      break
+  # Create empty numpy array for all the images to come
+  images = np.empty((len(os.listdir(folder)), img.shape[0], img.shape[1], img.shape[2]), dtype=np.float32)
+  
+  # Iterate over all filenames to fill in the images array
+  for i, filename in enumerate(os.listdir(folder)):
+    img = cv2.imread(os.path.join(folder,filename))
+    if img is not None:
+      images[i, ...] = img
+  if maxImg is not None:
+    maxImages = np.empty((maxImg, img.shape[0], img.shape[1], img.shape[2]), dtype=np.float32)
+    indices = np.random.permutation(len(os.listdir(folder)))
+    i = 0
+    for idx in indices:
+      maxImages[i, ...] = images[idx]
+      i+=1
+      if i>=maxImg:
+        break
+    return maxImages
+  else:
+    return images
+
+'''
+AUGMENT data further with multiple observations
+'''
+def makeObservations(imgList, mode='multi', farScale = 0.5, obliquePercentage = 10, obliqueDirection = 'left'):
+    """
+    Changes all the images in the provided list according to the given parameters.
+        imgList: [ndarray]
+            List of all images to be altered; All images must be of same dimensions
+        mode: string ('single' or 'multi')
+            If 'multi': ignores other parameters and generate 28 new observations for
+                        each image. 7 different oblique patterns each at 4 different
+                        distances (including original image too)
+            If 'single': create 1 observation for each image in the list according to
+                         the specified parameters
+        farScale: scalar (float) (0-1]
+            Parameter defining the closeness of the new image (1=same, ~0=farthest)
+        obliquePercentage: scalar (int) (0-45)
+            Defines how much the image is to be tilted
+        obliqueDirection: string ('left' or 'right')
+            Specifies the direction of tilt
+    """
+    if (len(imgList.shape)==3): #Only one image is provided
+        numImgs = 1
+        imgRows = imgList.shape[0]
+        imgCols = imgList.shape[1]
+        nChannels = imgList.shape[2]
+        imgList = [imgList]
+    else:
+        numImgs = imgList.shape[0]
+        imgRows = imgList.shape[1]
+        imgCols = imgList.shape[2]
+        nChannels = imgList.shape[3]
+    if mode=='multi':
+        obliquePercentages = [20, 30]
+        farScales = [1, 0.25]
+        newImgList = np.zeros((numImgs*((2*len(obliquePercentages))+1)*len(farScales), imgRows, imgCols, nChannels))
+        i = 0
+        for img in imgList:
+            for fS in farScales:
+                img1 = cv2.resize(img, (int(imgRows*fS), int(imgCols*fS)))
+                img1 = cv2.resize(img1, (imgRows, imgCols))
+                img1 = cv2.GaussianBlur(img1, (5,5), 0)
+                newImgList[i, ...] = img1
+                i+=1
+            for oP in obliquePercentages:
+                for fS in farScales:
+                    #For Left Oblique
+                    XobliquePixels = oP*imgRows/100
+                    YobliquePixels = 0.75*XobliquePixels
+                    src = np.array([
+                            [0, 0],
+                            [imgRows - 1, 0],
+                            [imgRows - 1, imgCols - 1],
+                            [0, imgCols - 1]
+                            ], dtype = "float32")
+                    dst = np.array([
+                            [XobliquePixels, YobliquePixels],
+                            [imgRows - XobliquePixels - 1, 0],
+                            [imgRows - XobliquePixels - 1, imgCols - 1],
+                            [XobliquePixels, imgCols - YobliquePixels - 1]
+                            ], dtype = "float32")
+                    # compute the perspective transform matrix and then apply it
+                    M = cv2.getPerspectiveTransform(src, dst)
+                    warped = cv2.warpPerspective(img, M, (imgRows, imgCols))
+                    #warped = warped[:, int(XobliquePixels):int(imgRows - XobliquePixels - 1)]
+                    warped = warped[int(oP*YobliquePixels/100):int(imgCols - (oP*YobliquePixels/100) - 1), 
+                                int(XobliquePixels):int(imgRows - XobliquePixels - 1)]
+                    warped = cv2.resize(warped, (imgRows, imgCols))
+                    img1 = cv2.resize(warped, (int(imgRows*fS), int(imgCols*fS)))
+                    img1 = cv2.resize(img1, (imgRows, imgCols))
+                    img1 = cv2.GaussianBlur(img1, (5,5), 0)
+                    newImgList[i, ...] = img1
+                    i+=1
+                    #For Right Oblique
+                    XobliquePixels = oP*imgRows/100
+                    YobliquePixels = 0.75*XobliquePixels
+                    src = np.array([
+                            [0, 0],
+                            [imgRows - 1, 0],
+                            [imgRows - 1, imgCols - 1],
+                            [0, imgCols - 1]
+                            ], dtype = "float32")
+                    dst = np.array([
+                            [XobliquePixels, 0],
+                            [imgRows - XobliquePixels - 1, YobliquePixels],
+                            [imgRows - XobliquePixels - 1, imgCols - YobliquePixels - 1],
+                            [XobliquePixels, imgCols - 1]
+                            ], dtype = "float32")
+                    # compute the perspective transform matrix and then apply it
+                    M = cv2.getPerspectiveTransform(src, dst)
+                    warped = cv2.warpPerspective(img, M, (imgRows, imgCols))
+                    warped = warped[int(oP*YobliquePixels/100):int(imgCols - (oP*YobliquePixels/100) - 1), 
+                                int(XobliquePixels):int(imgRows - XobliquePixels - 1)]
+                    warped = cv2.resize(warped, (imgRows, imgCols))
+                    img1 = cv2.resize(warped, (int(imgRows*fS), int(imgCols*fS)))
+                    img1 = cv2.resize(img1, (imgRows, imgCols))
+                    img1 = cv2.GaussianBlur(img1, (5,5), 0)
+                    newImgList[i, ...] = img1
+                    i+=1
+    else:
+        newImgList = np.zeros((numImgs, imgRows, imgCols, nChannels))
+        i = 0
+        for img in imgList:
+            XobliquePixels = obliquePercentage*imgRows/100
+            YobliquePixels = 0.75*XobliquePixels
+            if obliqueDirection=='left' or obliqueDirection == 'Left':
+                src = np.array([
+                    [0, 0],
+                    [imgRows - 1, 0],
+                    [imgRows - 1, imgCols - 1],
+                    [0, imgCols - 1]
+                    ], dtype = "float32")
+                dst = np.array([
+                    [XobliquePixels, YobliquePixels],
+                    [imgRows - XobliquePixels - 1, 0],
+                    [imgRows - XobliquePixels - 1, imgCols - 1],
+                    [XobliquePixels, imgCols - YobliquePixels - 1]
+                    ], dtype = "float32")
+            else:
+                src = np.array([
+                    [0, 0],
+                    [imgRows - 1, 0],
+                    [imgRows - 1, imgCols - 1],
+                    [0, imgCols - 1]
+                    ], dtype = "float32")
+                dst = np.array([
+                    [XobliquePixels, 0],
+                    [imgRows - XobliquePixels - 1, YobliquePixels],
+                    [imgRows - XobliquePixels - 1, imgCols - YobliquePixels - 1],
+                    [XobliquePixels, imgCols - 1]
+                    ], dtype = "float32")
+            # compute the perspective transform matrix and then apply it
+            M = cv2.getPerspectiveTransform(src, dst)
+            warped = cv2.warpPerspective(img, M, (imgRows, imgCols))
+            warped = warped[int(obliquePercentage*YobliquePixels/100):int(imgCols - (obliquePercentage*YobliquePixels/100) - 1), 
+                        int(XobliquePixels):int(imgRows - XobliquePixels - 1)]
+            warped = cv2.resize(warped, (imgRows, imgCols))
+            img1 = cv2.resize(warped, (int(imgRows*farScale), int(imgCols*farScale)))
+            img1 = cv2.resize(img1, (imgRows, imgCols))
+            img1 = cv2.GaussianBlur(img1, (5,5), 0)
+            newImgList[i, ...] = img1
+            i+=1
+    return newImgList
+
+'''
+CREATE Dataset
+'''
+folder = '../GTSRB Dataset/categorized&cropped_NA/'
+turnRight = load_images_from_folder(folder + '33')
+print("shape of original turnRight", turnRight.shape)
+turnLeft = load_images_from_folder(folder + '34')
+print("shape of original turnLeft", turnLeft.shape)
+goStraight = load_images_from_folder(folder + '35')
+print("shape of original goStraight", goStraight.shape)
+
+x_complete = np.concatenate((turnLeft, turnRight, goStraight))
+y_complete = np.zeros((x_complete.shape[0], 3))
+y_complete[0:turnLeft.shape[0], 0] = 1
+y_complete[turnLeft.shape[0]:turnLeft.shape[0] + turnRight.shape[0], 1] = 1
+y_complete[turnLeft.shape[0] + turnRight.shape[0]:, 2] = 1
+
+x_turnLeft = turnLeft
+y_turnLeft = np.zeros((x_turnLeft.shape[0], 3))
+y_turnLeft[:,0] = 1
+
+x_turnRight = turnRight
+y_turnRight = np.zeros((x_turnRight.shape[0], 3))
+y_turnRight[:,1] = 1
+
+x_goStraight = goStraight
+y_goStraight = np.zeros((x_goStraight.shape[0], 3))
+y_goStraight[:,2] = 1
+
+#modelPath = '../Models/ResNet20 Batch Max - Data Random - LR 1e-6/model.h5'
+modelPath = '../Models/VGG16 - Data Random - LR 1e-7/model.h5'
+
+with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+    model = load_model('model.h5')
+
+def pso_objective(features, *args):
+    xOffset, yOffset, scale, rotate = features
+    imgData, oriClass, tarClass, mudImgPath, model = args
+    mudSplatObj = mudSplat(mudImgPath, xOffset, yOffset, scale, rotate)
+    return predictModelMudSplat(imgData, oriClass, tarClass, model,
+                                [mudSplatObj])[0]
+
+'''
+For all images in same class - TurnLeft
+'''
+numSplats = [1]
+for num in numSplats:
+    for mudImgPath in ['AdversaryImages/mudSplat2.png', 'AdversaryImages/mudSplat1.png']:
+        testTLimgs = np.zeros(np.append(len(x_turnLeft), x_turnLeft[0].shape))
+        for j, img in enumerate(x_turnLeft):
+            testTLimgs = makeObservations(img)
+            oriClass = 0
+            tarClass = 1
+            args = (np.float32(testTLimgs), oriClass, tarClass, mudImgPath, model)
+            lb = [0.2*args[0][0].shape[0], 0.2*args[0][0].shape[1], 15, 0]
+            ub = [0.6*args[0][0].shape[0], 0.6*args[0][0].shape[1], 40, 360]
+            #q_opt, f_opt = pso(pso_objective, lb, ub, args=args,
+            #                   swarmsize=100, omega=0.8, phip=2.0, phig=2.0,
+            #                   maxiter=3000, minstep=1e-8, debug=True, processes=1)
+            q_opt, f_opt = pso(pso_objective, lb, ub, args=args,
+                               swarmsize=100, omega=0.8, phip=2.0, phig=2.0,
+                               maxiter=2, minstep=1e-8, debug=True)
+            print(q_opt)
+            print(f_opt)
+            print('Hooray!')
+            f = open("PSOoutput.txt", "a+")
+
+            f.write("For Turn Left Class (mudImgPath: " + mudImgPath + ") - image #" + str(j) + ":\n")
+            f.write("Original Accuracy: " + str(predictModelMudSplat(testTLimgs, oriClass, tarClass, model)[2]) + "\n")
+            f.write("q_opt = " + str(q_opt) + "\n")
+            f.write("f_opt = " + str(f_opt) + "\n")
+            mudSplatObject = mudSplat(mudImgPath, q_opt[0], q_opt[1], q_opt[2], q_opt[3])
+            f.write("New Accuracy: " + str(predictModelMudSplat(testTLimgs, oriClass, tarClass, model, [mudSplatObject])[2]) + "\n\n")
+            f.close()
---
+++ Final Code/utils_naturalPerturbations.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 22:14:00 2019
+
+@author: Mayank Jain
+"""
+
+import numpy as np
+import cv2
+import math
+
+def precalc_gradtable(gradtable, W, H, randSeed=np.random.randint(0,(2**16)-1)):
+    rnd = np.random.RandomState(seed = randSeed)
+    for i in range(0,H):
+        for j in range(0, W):
+            x = float((rnd.randint(1,2*W))-W)/W
+            y = float((rnd.randint(1,2*H))-H)/H
+            s = math.sqrt(x * x + y * y)
+            if s!=0:
+                x = x / s
+                y = y / s
+            else:
+                x = 0
+                y = 0
+            gradtable[i*H+j] = (x,y)
+    return gradtable
+
+#calculate dot product for v1 and v2
+def dot(v1,v2):
+    return ( (v1[0]*v2[0]) + (v1[1]*v2[1]) )
+
+# get a pseudorandom gradient vector
+def gradient(x,y, gradtable, W, H):
+    # normalize!
+    return gradtable[y*H+x]
+
+def s_curve(x):
+    return ( 3*x*x - 2*x*x*x )
+
+def noise2d(x, y, gradtable, W, H):
+    x0 = math.floor(x)
+    y0 = math.floor(y)
+    x1 = x0 + 1.0
+    y1 = y0 + 1.0
+    
+    i_x0 = int(x0)
+    i_x1 = int(x1)
+    i_y0 = int(y0)
+    i_y1 = int(y1)
+    
+    s = dot(gradient(i_x0, i_y0, gradtable, W, H),(x-x0, y-y0))
+    t = dot(gradient(i_x1, i_y0, gradtable, W, H),(x-x1, y-y0))
+    u = dot(gradient(i_x0, i_y1, gradtable, W, H),(x-x0, y-y1))
+    v = dot(gradient(i_x1, i_y1, gradtable, W, H),(x-x1, y-y1))
+    
+    s_x = s_curve( x - x0 )
+    a = s + s_x*t - s_x*s
+    b = u + s_x*v - s_x*u
+    
+    s_y = s_curve( y - y0 )
+    z = a + s_y*b - s_y*a
+    
+    return z
+
+def col( a ):
+    return int(round((128-(128*a))))
+
+def scaleTo8Bit(image, displayMin = None, displayMax=None ):
+    if displayMin == None:
+        displayMin = np.min(image)
+    
+    if displayMax == None:
+        displayMax = np.max(image)
+    
+    np.clip(image, displayMin, displayMax, out=image)
+    
+    image = image - displayMin
+    cf = 255. / (displayMax - displayMin)
+    imageOut = ( cf * image).astype(np.uint8)
+    
+    return imageOut
+
+def generatePerlin(W, H, randomSeed):
+    gradtable = [ (0,0) for i in range(0,W*H) ]
+    
+    gradtable = precalc_gradtable(gradtable, W, H, randomSeed)
+    
+    perlin = np.zeros((W,H))
+    
+    zoom_x = 0.2
+    zoom_y = 0.2
+    x = 0.0
+    y = 0.0
+    for j in range(0,H):
+        for i in range(0,W):
+            a = col(noise2d(x,y,gradtable,W,H))
+            perlin[i,j] = a
+            x = x + zoom_x
+        y = y + zoom_y
+        x = 0.0
+    perlin = cv2.GaussianBlur(perlin, (int(W/2)+1,int(H/2)+1), 0)
+    return perlin
+
+def addFog(imageList, fogIntensity=0.8, randomSeed=10):
+    """
+    Adds foggy effect to the image given as input.
+    
+    Arguments:
+    -----------
+        imageList: np.ndarray (numImages, Width, Height, numChannels)
+            List of images on which fog is required to be added
+        fogIntensity: float (0-1)
+            Defines the intensity or thickness of the fog. Higher this number,
+            higher the thickness.
+        randomSeed: +ve int (uint16)
+            Specifies the randomSeed for generating Perlin's Noise which will
+            be used to add fog later. In other words, it alters the orientation
+            of fog particles
+    Returns:
+    -----------
+        fogImgs: np.ndarray (numImages, Width, Height, numChannels)
+            The list of transformed output images with foggy effect
+    """
+    if len(imageList.shape)==3:
+        imageList = np.expand_dims(imageList, axis=0)
+    numImgs = imageList.shape[0]
+    W = imageList.shape[1]
+    H = imageList.shape[2]
+    nCh = imageList.shape[3]
+    
+    perlin = scaleTo8Bit(generatePerlin(W, H, randomSeed))
+    cv2.imwrite('perlinFog.png', perlin)
+    fogImgs = np.zeros((numImgs,W,H,nCh))
+    for n in range(nCh):
+        fogImgs[:,:,:,n] = np.add((fogIntensity*perlin), np.multiply(1-(fogIntensity*perlin/255), imageList[:,:,:,n]))
+    return fogImgs.astype('uint8')
+
+def addRain(imageList, randomSeed=10, mode='withMist'):
+    """
+    Adds rain effect on glass to the image given as input.
+    
+    Arguments:
+    -----------
+        imageList: np.ndarray (numImages, Width, Height, numChannels)
+            List of images on which raindrop effect is required to be added
+        randomSeed: +ve int (uint16)
+            Specifies the randomSeed for generating Perlin's Noise which will
+            be used to add rain drops later. In other words, it alters the
+            location of rain drops.
+        mode: string ('withMist' or 'noMist')
+            If 'withMist': adds a gaussian blur to original image before adding rain drops
+            If 'noMist':   no effect of blur/mist on original image
+    Returns:
+    -----------
+        rainImgs: np.ndarray (numImages, Width, Height, numChannels)
+            The list of transformed output images with raindrops effect
+    """
+    if len(imageList.shape)==3:
+        imageList = np.expand_dims(imageList, axis=0)
+    numImgs = imageList.shape[0]
+    W = imageList.shape[1]
+    H = imageList.shape[2]
+    nCh = imageList.shape[3]
+    rainImgs = np.zeros((numImgs,W,H,nCh))
+    for j,image in enumerate(imageList):
+        if (mode=='withMist'):
+            image[:,:,:3] = cv2.GaussianBlur(image[:,:,:3], (5,5), 5)
+        perlin = generatePerlin(W, H, randomSeed)
+        cv2.imwrite('perlinRain.png', perlin)
+        perlin_thr = cv2.threshold(perlin,127,255,cv2.THRESH_BINARY_INV)[1]
+        alpha = perlin_thr==255
+        alpha = 255*alpha.astype(int)
+        perlin_thr = cv2.merge((perlin_thr.astype(int), perlin_thr.astype(int), perlin_thr.astype(int), alpha))
+        rainDrops = cv2.GaussianBlur(scaleTo8Bit(perlin_thr), (3,3), 0)
+        imgIn = rainDrops[:,:,:-1].astype(np.float32)
+        #Create the  filter
+        kernel = np.zeros( (1,2), np.float32)
+        kernel[0,0] = -3.0  #Try 2.0
+        kernel[0,1] = 3.0   #Try 2.0
+        
+        #Do the actual kernel operation...
+        output = cv2.filter2D(imgIn.astype(np.float32), -1, kernel)
+        #Scaling back so that the zero point is at 128 (gray)...
+        output8bit = scaleTo8Bit(output)
+        output8bit = cv2.GaussianBlur(output8bit, (3,3), 0)
+        alpha[alpha==255]=100
+        alpha_s = alpha/255
+        alpha_l = 1.0 - alpha_s
+        for i in range(nCh):
+            if(i<3):
+                rainImgs[j,:,:,i] = np.add(np.multiply(image[:,:,i], alpha_l), np.multiply(output8bit[:,:,i], alpha_s))
+            else:
+                rainImgs[j,:,:,i] = image[:,:,i]
+    return rainImgs
+
+def addAlpha(imageList):
+    newImageList = np.zeros((len(imageList), imageList[0].shape[0], imageList[0].shape[1], 4))
+    for i, image in enumerate(imageList):
+        if image.shape[2]==3:
+            newImageList[i,...] = np.dstack((image, 255*np.ones((image.shape[0],image.shape[1]))))
+        else:
+            newImageList[i,...] = image
+    return newImageList
