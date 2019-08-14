403b5c6badc19828dcc73eec9c924b15db2e8abb
Mayank Jain <jain15mayank@gmail.com>
2019-08-14 15:52:24 +0100

    Attempt to parallize using joblib

--- Final Code/Real World Experiment/pso_pyswarm_parallel.py
+++ Final Code/Real World Experiment/pso_pyswarm_parallel.py
@@ -246,9 +246,10 @@ def predictModel_Nparticles(originalImages, originalClass, targetClass,
     else:
         numFinImages = numImgs*len(alterFeatures)
         finImages = np.zeros((numFinImages, W, H, nCh))
-        finImages = np.array(Parallel(n_jobs=40)(delayed(alterImages)(originalImages, feature) for n,feature in enumerate(alterFeatures)))
-        finImages = np.reshape(finImages, (finImages.shape[0]*finImages.shape[1], finImages.shape[2], finImages.shape[3]))
-        print(finImages.shape)
+        finImages = np.concatenate((Parallel(n_jobs=40)(delayed(alterImages)(originalImages, feature) for n,feature in enumerate(alterFeatures))), axis=0)
+        print(finImages.shape) 
+        #finImages = np.reshape(finImages, (finImages.shape[0]*finImages.shape[1], finImages.shape[2], finImages.shape[3], finImages.shape[4]))
+        #print(finImages.shape)
         '''
         for n, feature in enumerate(alterFeatures):
             print("Adding Mud-Splats")
